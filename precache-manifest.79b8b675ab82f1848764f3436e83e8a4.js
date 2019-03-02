59131c53fa894ba91b2262780eb7d929645ec376
Jared Wheet <jaredwheet@gmail.com>
2019-03-01 20:48:09 -0700

    Updates

---
+++ precache-manifest.79b8b675ab82f1848764f3436e83e8a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-memory-game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a2ed142be53bff71048f",
+    "url": "/react-memory-game/static/js/runtime~main.a2ed142b.js"
+  },
+  {
+    "revision": "2f6b787840a84f4d0662",
+    "url": "/react-memory-game/static/js/main.2f6b7878.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/react-memory-game/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "2f6b787840a84f4d0662",
+    "url": "/react-memory-game/static/css/main.714452c8.chunk.css"
+  },
+  {
+    "revision": "fd6f9d7f632b9dd7207b0bd3b3d66fc0",
+    "url": "/react-memory-game/index.html"
+  }
+];
